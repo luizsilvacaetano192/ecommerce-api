8d8b2785e9b1f30368fe850db28e5b1b4a03298d
Luiz caetano <luizsilvacaetano192@gmail.com>
2025-08-12 19:29:24 -0300

    feat(migration): criar migration da tabela orders com índices e relacionamentos

---
+++ database/migrations/2025_08_12_222540_create_orders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table): void {
+            $table->id();
+            
+            $table->foreignId('user_id')->constrained()->onDelete('cascade')->index();
+            
+            $table->string('description')->index();
+            
+            $table->decimal('value', 10, 2)->index();
+            
+            $table->enum('currency', ['BRL', 'USD'])->index();
+            
+            $table->timestamps();
+            
+            $table->index('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+}
